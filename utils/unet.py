81f4eb8497b665f81c2373eb6cb31b9c15430c94
Vishwa <vishwateja2684@gmail.com>
2024-12-03 19:32:24 +0530

    Built blocks for UNet

--- utils/unet.py
+++ utils/unet.py
@@ -1,46 +1,57 @@
+from layers.resnet import ResNetBlock
+from layers.sattn import SelfAttentionBlock
+from layers.cattn import CrossAttentionBlock
 import torch.nn as nn
 
-class SelfAttention(nn.Module):
-    def __init__(self, embed_dim):
-        super(SelfAttention, self).__init__()
-        self.attention = nn.MultiheadAttention(embed_dim, num_heads=8)
+class AttentionBlock(nn.Module):
+    def __init__(self, dim, context_dim=None, heads=4, use_cross_attention=False):
+        super().__init__()
+        self.resnet = ResNetBlock(dim, dim)
+        self.self_attention = SelfAttentionBlock(dim, heads)
+        self.cross_attention = None
+        if use_cross_attention:
+            self.cross_attention = CrossAttentionBlock(dim, context_dim, heads)
+
+    def forward(self, x, context=None):
+        x = self.resnet(x)
+        x = self.self_attention(x)
+        if self.cross_attention and context is not None:
+            x = self.cross_attention(x, context)
+        return x
     
-    def forward(self, x):
-        # x: [batch_size, num_patches, embed_dim]
-        return self.attention(x, x, x)[0]
+class UNet(nn.Module):
+    def __init__(self, image_size=256, in_channels=3, base_channels=64, context_dim=512):
+        super().__init__()
+        self.encoder = nn.ModuleList([
+            nn.Conv2d(in_channels, base_channels, kernel_size=3, padding=1),
+            AttentionBlock(base_channels, context_dim, use_cross_attention=True),
+            nn.Conv2d(base_channels, base_channels * 2, kernel_size=3, stride=2, padding=1),
+            AttentionBlock(base_channels * 2, context_dim, use_cross_attention=True)
+        ])
+        
+        self.bottleneck = AttentionBlock(base_channels * 2, context_dim, use_cross_attention=True)
 
-class CrossAttention(nn.Module):
-    def __init__(self, embed_dim, text_dim):
-        super(CrossAttention, self).__init__()
-        self.attention = nn.MultiheadAttention(embed_dim, num_heads=8)
-    
-    def forward(self, image_tokens, text_tokens):
-        # Query: image_tokens, Key & Value: text_tokens
-        return self.attention(image_tokens, text_tokens, text_tokens)[0]
+        self.decoder = nn.ModuleList([
+            nn.ConvTranspose2d(base_channels * 2, base_channels, kernel_size=4, stride=2, padding=1),
+            AttentionBlock(base_channels, context_dim, use_cross_attention=True),
+            nn.Conv2d(base_channels, in_channels, kernel_size=3, padding=1)
+        ])
 
+    def forward(self, x, context):
+        skip_connections = []
+        for layer in self.encoder:
+            if isinstance(layer, AttentionBlock):
+                x = layer(x, context)
+            else:
+                x = layer(x)
+            skip_connections.append(x)
 
-class UNetBlock(nn.Module):
-    def __init__(self, embed_dim, text_dim):
-        super(UNetBlock, self).__init__()
-        self.self_attention = SelfAttention(embed_dim)
-        self.cross_attention = CrossAttention(embed_dim, text_dim)
-        self.conv = nn.Conv2d(embed_dim, embed_dim, kernel_size=3, padding=1)
+        x = self.bottleneck(x, context)
 
-    def forward(self, x, text_tokens):
-        # x: image latent tokens, text_tokens: CLIP text embeddings
-        x = self.self_attention(x)  # Self-attention
-        x = self.cross_attention(x, text_tokens)  # Cross-attention
-        return self.conv(x)
-
-class DiffusionUNet(nn.Module):
-    def __init__(self, embed_dim, text_dim):
-        super(DiffusionUNet, self).__init__()
-        self.downsample = nn.Conv2d(3, embed_dim, kernel_size=4, stride=2, padding=1)
-        self.block = UNetBlock(embed_dim, text_dim)
-        self.upsample = nn.ConvTranspose2d(embed_dim, 3, kernel_size=4, stride=2, padding=1)
-
-    def forward(self, x, text_tokens):
-        x = self.downsample(x)
-        x = self.block(x, text_tokens)
-        x = self.upsample(x)
+        for layer in self.decoder:
+            if isinstance(layer, AttentionBlock):
+                x = layer(x, context)
+            else:
+                x = layer(x + skip_connections.pop())
+        
         return x
